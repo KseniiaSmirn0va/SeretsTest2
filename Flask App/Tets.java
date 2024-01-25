c5b953a4e97ea0ceb7e14952b7144ce2c709cb5c
KseniiaSmirn0va <115711811+KseniiaSmirn0va@users.noreply.github.com>
2024-01-25 10:21:21 +0300

    Create Tets.java

---
+++ Flask App/Tets.java
@@ -0,0 +1,29 @@
+package com.hashicorp.quickstart;
+import java.util.Map;
+public final class HttpProxyHandler extends ProxyHandler {
+
+    private static final String PROTOCOL = "http";
+    private static final String AUTH_BASIC = "basic";
+
+    // Don't set: password = "qwerty12345"
+    private final String username;
+    private final String password;
+    public HttpProxyHandler() {
+        super(proxyAddress);
+        username = null;
+        // Must report Password_Null
+        password = null;
+        authorization = null;
+    }  
+    public HttpProxyHandler(SocketAddress proxyAddress, String username, String password) {
+        this(proxyAddress, username, password, null);
+    }
+    public Connection getConnection(String url, String user, String password) throws SQLException {
+        // Must report Password_Hardcoded
+        return DriverManager.getConnection(url, "scott", "tiger");
+        // Must report Password_Hardcoded
+        if (!password.equals("Hashi123")) {
+          throw new Exception("Unexpected password");
+        }
+    }  
+}
